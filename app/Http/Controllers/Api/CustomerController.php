cf01449320627ebd0f995ac09d70fb46f937c4c4
jfkeci <ja.lip132@gmail.com>
2023-03-05 11:33:01 +0100

    feat: paginated customers endpoint - add customer collection

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCustomerRequest;
@@ -14,7 +16,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
@@ -38,7 +40,7 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
